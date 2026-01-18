c01950ad63ea897f2891bd46a50d7545ecb3b44c
Tess224 <127836880+Tess224@users.noreply.github.com>
2026-01-18 01:17:45 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         </div>
         
         <Header />
-        <main className="relative z-10 bg-grid">
+        <main className="relative z-10 bg-grid bg-clip-padding px-6">
   {children}
 </main>
         <Footer />
